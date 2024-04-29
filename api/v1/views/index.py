5a515b3eb1da15f2ecd70464d057a2578713f55c
Lagat100 <weldonkiprop1@gmail.com>
2024-04-29 02:48:26 -0700

    index

---
+++ api/v1/views/index.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Index module """
+
+from api.v1.views import app_views
+from flask import jsonify
+
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status():
+    """ Return status: OK """
+    return jsonify({"status": "OK"})
