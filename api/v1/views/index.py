44df3289953eb1f2a1b519cac12d88856a602ad3
Lagat100 <weldonkiprop1@gmail.com>
2024-04-29 02:52:49 -0700

    index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -9,3 +9,17 @@ from flask import jsonify
 def status():
     """ Return status: OK """
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stats():
+    """ Retrieves the number of each objects by type """
+    classes = {
+        "Amenity": storage.count("Amenity"),
+        "City": storage.count("City"),
+        "Place": storage.count("Place"),
+        "Review": storage.count("Review"),
+        "State": storage.count("State"),
+        "User": storage.count("User")
+    }
+    return jsonify(classes)
